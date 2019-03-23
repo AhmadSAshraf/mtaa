d845b8b6abd64d67ba00d8e4da9383295e4dff6f
Fahari <karonjekevin67@gmail.com>
2019-03-23 22:58:20 +0300

    solved user object has no attribute profile

--- mtaa/models.py
+++ mtaa/models.py
@@ -10,7 +10,7 @@ class Neighbourhood(models.Model):
 class Profile(models.Model):
     image=models.ImageField(default='default.jpg', upload_to='profile_pics')
     bio=models.CharField(max_length=300)
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.user.username} Profile'
