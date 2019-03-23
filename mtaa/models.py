a396b843f77618dcbdd427d8c10307f440bb8d0b
Fahari <karonjekevin67@gmail.com>
2019-03-23 20:41:22 +0300

    create neighbourhood model

--- mtaa/models.py
+++ mtaa/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Neighbourhood(models.Model):
+    name=models.CharField(max_length=60)
+    location=models.CharField(max_length=60)
+    population=models.IntegerField()
+    admin=models.ForeignKey(User,on_delete=models.CASCADE)
