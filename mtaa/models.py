3f8b4aa501bcbca0511c113f69afde1dc9b2866d
Fahari <karonjekevin67@gmail.com>
2019-03-23 21:55:11 +0300

    create profile model

--- mtaa/models.py
+++ mtaa/models.py
@@ -1,8 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Neighbourhood(models.Model):
     name=models.CharField(max_length=60)
     location=models.CharField(max_length=60)
     population=models.IntegerField()
     admin=models.ForeignKey(User,on_delete=models.CASCADE)
+
+class Profile(models.Model):
+    profile_pic=models.ImageField(upload_to='profile_photos/')
+    bio=models.CharField(max_length=300)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
