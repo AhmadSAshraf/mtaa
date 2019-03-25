b8f65cdba7202203d2689fc580fafe2d8cfa1d9c
Fahari <karonjekevin67@gmail.com>
2019-03-25 12:15:05 +0300

    takes you to details page but post and businesses dont display

--- mtaa/views.py
+++ mtaa/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
 from . models import *
 from .forms import *
+from django.views import generic
 
 
 # Create your views here.
@@ -63,3 +64,38 @@ def neighbourhood_details(request,neighbourhood_id):
     posts=Post.objects.filter(neighborhood=neighbourhood_id)
     neighbourhood=Neighbourhood.objects.get(pk=neighbourhood_id)
     return render(request,'details.html',{'neighbourhood':neighbourhood,'businesses':businesses,'posts':posts})
+
+
+@login_required(login_url="/accounts/login/")
+def new_business(request,pk):
+    current_user = request.user
+    neighborhood = get_object_or_404(Neighbourhood,pk=pk)
+    if request.method == 'POST':
+        business_form = NewBusinessForm(request.POST, request.FILES)
+        if business_form.is_valid():
+            business = business_form.save(commit=False)
+            business.user = current_user
+            business.neighborhood=neighborhood
+            business.save()
+        return redirect('detail', neighbourhood_id=neighborhood.id)
+
+    else:
+        business_form = NewBusinessForm()
+    return render(request, 'new_business_form.html', {"form": business_form,'neighborhood':neighborhood})
+
+@login_required(login_url="/accounts/login/")
+def new_post(request,pk):
+    current_user = request.user
+    neighborhood = get_object_or_404(Neighbourhood,pk=pk)
+    if request.method == 'POST':
+        post_form = NewPostForm(request.POST, request.FILES)
+        if post_form.is_valid():
+            post = post_form.save(commit=False)
+            post.user = current_user
+            post.neighborhood=neighborhood
+            post.save()
+        return redirect('detail', neighbourhood_id=neighborhood.id)
+
+    else:
+        post_form = NewPostForm()
+    return render(request, 'new_post_form.html', {"form": post_form,'neighborhood':neighborhood})
